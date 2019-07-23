a01f02c98d9100c6ede2ff11ffba95168d39c43d
Sean <sean@celsoft.com>
2019-07-22 20:35:18 -0700

    Add y parameter to useQueryParam hook example

--- src/pages/query_params_example/index.tsx
+++ src/pages/query_params_example/index.tsx
@@ -4,14 +4,16 @@ import useQueryParam from '../../hooks/use-query-params';
 
 export function Page() {
   const [x, setX] = useQueryParam('x');
+  const [y, setY] = useQueryParam('y');
 
   return (
     <div>
       <h4>Use Query Params Example</h4>
 
       <p>x: {x}</p>
+      <p>y: {y}</p>
 
-      <button onClick={() => setX(Math.random().toString())}>Change</button>
+      <button onClick={() => {setX(Math.random().toString()); setY(Math.random().toString())}}>Change</button>
       <button onClick={() => setX(undefined)}>Undefine</button>
     </div>
   );
